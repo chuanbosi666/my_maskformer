edbe5ec49bdc479672959c8e6ab9b5041148ab59
chuanbosi <chuanbosi001@gmail.com>
2023-09-24 10:29:35 +0800

    updata transformer.py annotation

--- mask_former/modeling/transformer/transformer.py
+++ mask_former/modeling/transformer/transformer.py
@@ -16,7 +16,7 @@ import torch.nn.functional as F
 from torch import Tensor, nn
 
 
-class Transformer(nn.Module):
+class Transformer(nn.Module):  # 构建Transformer模块
     def __init__(
         self,
         d_model=512,
@@ -33,9 +33,9 @@ class Transformer(nn.Module):
 
         encoder_layer = TransformerEncoderLayer(
             d_model, nhead, dim_feedforward, dropout, activation, normalize_before
-        )
-        encoder_norm = nn.LayerNorm(d_model) if normalize_before else None
-        self.encoder = TransformerEncoder(encoder_layer, num_encoder_layers, encoder_norm)
+        )  # 调用TransformerEncoderLayer构建encoder_layer
+        encoder_norm = nn.LayerNorm(d_model) if normalize_before else None  # 如果normalize_before为True，则使用nn.LayerNorm(d_model)，否则为None
+        self.encoder = TransformerEncoder(encoder_layer, num_encoder_layers, encoder_norm)  # 调用TransformerEncoder构建encoder
 
         decoder_layer = TransformerDecoderLayer(
             d_model, nhead, dim_feedforward, dropout, activation, normalize_before
@@ -46,9 +46,9 @@ class Transformer(nn.Module):
             num_decoder_layers,
             decoder_norm,
             return_intermediate=return_intermediate_dec,
-        )
+        )  # 同上
 
-        self._reset_parameters()
+        self._reset_parameters()  # 初始化参数
 
         self.d_model = d_model
         self.nhead = nhead
@@ -60,25 +60,25 @@ class Transformer(nn.Module):
 
     def forward(self, src, mask, query_embed, pos_embed):
         # flatten NxCxHxW to HWxNxC
-        bs, c, h, w = src.shape
-        src = src.flatten(2).permute(2, 0, 1)
+        bs, c, h, w = src.shape  # 获取src的维度
+        src = src.flatten(2).permute(2, 0, 1)  # 将src的维度变为seq_length*bs*d_model，符合多头注意力机制的要求
         pos_embed = pos_embed.flatten(2).permute(2, 0, 1)
-        query_embed = query_embed.unsqueeze(1).repeat(1, bs, 1)
-        if mask is not None:
+        query_embed = query_embed.unsqueeze(1).repeat(1, bs, 1)  # 同理，将query_embed的维度变为num_queries*bs*d_model，符合多头注意力机制的要求
+        if mask is not None:  # 如果mask不为None，则将其维度变为bs*seq_length
             mask = mask.flatten(1)
 
-        tgt = torch.zeros_like(query_embed)
-        memory = self.encoder(src, src_key_padding_mask=mask, pos=pos_embed)
+        tgt = torch.zeros_like(query_embed)  # 根据query_embed的维度，构建一个全零的tensor
+        memory = self.encoder(src, src_key_padding_mask=mask, pos=pos_embed)  # 将src传入encoder，得到memory输出
         hs = self.decoder(
             tgt, memory, memory_key_padding_mask=mask, pos=pos_embed, query_pos=query_embed
-        )
-        return hs.transpose(1, 2), memory.permute(1, 2, 0).view(bs, c, h, w)
+        )  # 将tgt和memory传入decoder，得到hs输出
+        return hs.transpose(1, 2), memory.permute(1, 2, 0).view(bs, c, h, w)  # 将hs的维度变为bs*num_queries*seq_length，将memory的维度变回为[bs, c, h, w]
 
 
 class TransformerEncoder(nn.Module):
     def __init__(self, encoder_layer, num_layers, norm=None):
         super().__init__()
-        self.layers = _get_clones(encoder_layer, num_layers)
+        self.layers = _get_clones(encoder_layer, num_layers)  # 使用_get_clones函数复制encoder_layer，复制的次数是num_layers
         self.num_layers = num_layers
         self.norm = norm
 
@@ -151,7 +151,7 @@ class TransformerDecoder(nn.Module):
         return output.unsqueeze(0)
 
 
-class TransformerEncoderLayer(nn.Module):
+class TransformerEncoderLayer(nn.Module):  # 构建Encoder层
     def __init__(
         self,
         d_model,
@@ -162,11 +162,11 @@ class TransformerEncoderLayer(nn.Module):
         normalize_before=False,
     ):
         super().__init__()
-        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
+        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)  # 多头注意力机制
         # Implementation of Feedforward model
-        self.linear1 = nn.Linear(d_model, dim_feedforward)
-        self.dropout = nn.Dropout(dropout)
-        self.linear2 = nn.Linear(dim_feedforward, d_model)
+        self.linear1 = nn.Linear(d_model, dim_feedforward)  # 线性层
+        self.dropout = nn.Dropout(dropout)  # dropout层
+        self.linear2 = nn.Linear(dim_feedforward, d_model)  # 线性层
 
         self.norm1 = nn.LayerNorm(d_model)
         self.norm2 = nn.LayerNorm(d_model)
@@ -174,9 +174,9 @@ class TransformerEncoderLayer(nn.Module):
         self.dropout2 = nn.Dropout(dropout)
 
         self.activation = _get_activation_fn(activation)
-        self.normalize_before = normalize_before
+        self.normalize_before = normalize_before  # 是否进行层归一化
 
-    def with_pos_embed(self, tensor, pos: Optional[Tensor]):
+    def with_pos_embed(self, tensor, pos: Optional[Tensor]):  # 将位置编码加到输入中
         return tensor if pos is None else tensor + pos
 
     def forward_post(
@@ -186,15 +186,15 @@ class TransformerEncoderLayer(nn.Module):
         src_key_padding_mask: Optional[Tensor] = None,
         pos: Optional[Tensor] = None,
     ):
-        q = k = self.with_pos_embed(src, pos)
+        q = k = self.with_pos_embed(src, pos)   # 将位置编码加到输入中，获得q和k  对于src来说，其是一个二维的矩阵，其维度是seq_length*d_model，这样是为了满足多头注意力机制的要求
         src2 = self.self_attn(
             q, k, value=src, attn_mask=src_mask, key_padding_mask=src_key_padding_mask
-        )[0]
-        src = src + self.dropout1(src2)
-        src = self.norm1(src)
-        src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
-        src = src + self.dropout2(src2)
-        src = self.norm2(src)
+        )[0]  # 多头注意力机制，传进的维度是q，k代表的num_heads的数目，value代表的是src，attn_mask代表的是src_mask，key_padding_mask代表的是src_key_padding_mask
+        src = src + self.dropout1(src2)  # 对其进行残差链接
+        src = self.norm1(src)  #归一化
+        src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))  # 先对未加入位置编码的src进行线性变换，然后进行激活函数，再进行dropout，再进行线性变换
+        src = src + self.dropout2(src2)  # 再进行残差链接
+        src = self.norm2(src)  # 再进行层归一化
         return src
 
     def forward_pre(
@@ -204,8 +204,8 @@ class TransformerEncoderLayer(nn.Module):
         src_key_padding_mask: Optional[Tensor] = None,
         pos: Optional[Tensor] = None,
     ):
-        src2 = self.norm1(src)
-        q = k = self.with_pos_embed(src2, pos)
+        src2 = self.norm1(src) #对src进行层归一化
+        q = k = self.with_pos_embed(src2, pos)  # 对归一化之后的src加入位置编码，获得q和k
         src2 = self.self_attn(
             q, k, value=src2, attn_mask=src_mask, key_padding_mask=src_key_padding_mask
         )[0]
@@ -222,12 +222,12 @@ class TransformerEncoderLayer(nn.Module):
         src_key_padding_mask: Optional[Tensor] = None,
         pos: Optional[Tensor] = None,
     ):
-        if self.normalize_before:
+        if self.normalize_before:  # 会根据在加入位置编码之前还是之后继续层归一化，来选择forward_pre还是forward_post，唯一的区别就是forward_pre会先进行层归一化，再进行多头注意力机制，而forward_post会先进行多头注意力机制，再进行层归一化
             return self.forward_pre(src, src_mask, src_key_padding_mask, pos)
         return self.forward_post(src, src_mask, src_key_padding_mask, pos)
 
 
-class TransformerDecoderLayer(nn.Module):
+class TransformerDecoderLayer(nn.Module):  # 构建Decoder层
     def __init__(
         self,
         d_model,
@@ -245,14 +245,14 @@ class TransformerDecoderLayer(nn.Module):
         self.dropout = nn.Dropout(dropout)
         self.linear2 = nn.Linear(dim_feedforward, d_model)
 
-        self.norm1 = nn.LayerNorm(d_model)
+        self.norm1 = nn.LayerNorm(d_model)  # 层归一化和残差链接
         self.norm2 = nn.LayerNorm(d_model)
         self.norm3 = nn.LayerNorm(d_model)
         self.dropout1 = nn.Dropout(dropout)
         self.dropout2 = nn.Dropout(dropout)
         self.dropout3 = nn.Dropout(dropout)
 
-        self.activation = _get_activation_fn(activation)
+        self.activation = _get_activation_fn(activation)  # 激活函数
         self.normalize_before = normalize_before
 
     def with_pos_embed(self, tensor, pos: Optional[Tensor]):
@@ -261,7 +261,7 @@ class TransformerDecoderLayer(nn.Module):
     def forward_post(
         self,
         tgt,
-        memory,
+        memory,  # memory是encoder的输出
         tgt_mask: Optional[Tensor] = None,
         memory_mask: Optional[Tensor] = None,
         tgt_key_padding_mask: Optional[Tensor] = None,
@@ -272,24 +272,24 @@ class TransformerDecoderLayer(nn.Module):
         q = k = self.with_pos_embed(tgt, query_pos)
         tgt2 = self.self_attn(
             q, k, value=tgt, attn_mask=tgt_mask, key_padding_mask=tgt_key_padding_mask
-        )[0]
-        tgt = tgt + self.dropout1(tgt2)
-        tgt = self.norm1(tgt)
+        )[0]  # 取出多头注意力机制的第一个值
+        tgt = tgt + self.dropout1(tgt2)  # 进行残差链接
+        tgt = self.norm1(tgt)  # 归一化后的tgt
         tgt2 = self.multihead_attn(
             query=self.with_pos_embed(tgt, query_pos),
             key=self.with_pos_embed(memory, pos),
             value=memory,
             attn_mask=memory_mask,
             key_padding_mask=memory_key_padding_mask,
-        )[0]
-        tgt = tgt + self.dropout2(tgt2)
-        tgt = self.norm2(tgt)
-        tgt2 = self.linear2(self.dropout(self.activation(self.linear1(tgt))))
+        )[0]  # 将q,k,v分别传入多头注意力机制，取出第一个值
+        tgt = tgt + self.dropout2(tgt2)  # 再和tgt进行残差链接
+        tgt = self.norm2(tgt)  # 再进行归一化
+        tgt2 = self.linear2(self.dropout(self.activation(self.linear1(tgt))))  # 先对未加入位置编码的tgt进行线性变换，然后进行激活函数，再进行dropout，再进行线性变换
         tgt = tgt + self.dropout3(tgt2)
         tgt = self.norm3(tgt)
         return tgt
 
-    def forward_pre(
+    def forward_pre(  # 同上
         self,
         tgt,
         memory,
@@ -331,7 +331,7 @@ class TransformerDecoderLayer(nn.Module):
         pos: Optional[Tensor] = None,
         query_pos: Optional[Tensor] = None,
     ):
-        if self.normalize_before:
+        if self.normalize_before:  # 还是根据在加入位置编码之前还是之后继续层归一化，来选择forward_pre还是forward_post，唯一的区别就是forward_pre会先进行层归一化，再进行多头注意力机制，而forward_post会先进行多头注意力机制，再进行层归一化
             return self.forward_pre(
                 tgt,
                 memory,
